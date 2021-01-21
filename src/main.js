b121e58afd64f75fc6b7e5518a7115d50bfdf16a
jagtrrz <jagutierrezc7@gmail.com>
2021-01-21 12:06:41 +0000

    create a new card when refresh

--- src/main.js
+++ src/main.js
@@ -4,7 +4,45 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+const allCards = ["&spades;", "&clubs;", "&diams;", "&hearts;"];
+const cardHeight = [
+  "A",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const topIcon = document.querySelector("#topIcon");
+const bottomIcon = document.querySelector("#bottomIcon");
+const numberCard = document.querySelector("#numberCard");
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let myRand = randomSuit();
+  changeIcons(topIcon, myRand);
+  changeIcons(bottomIcon, myRand);
+  changeCardHeight(numberCard);
 };
+function randomSuit() {
+  return Math.floor(Math.random() * allCards.length);
+}
+
+function changeIcons(iconID, myRand) {
+  iconID.innerHTML = allCards[myRand];
+  if (myRand == 0) iconID.classList.add("spade");
+  if (myRand == 1) iconID.classList.add("club");
+  if (myRand == 2) iconID.classList.add("diamond");
+  if (myRand == 3) iconID.classList.add("heart");
+}
+
+function changeCardHeight(card) {
+  let randomCardHeight = Math.floor(Math.random() * cardHeight.length);
+  card.innerHTML = cardHeight[randomCardHeight];
+}
